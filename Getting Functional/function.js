2fa1fdda9a88e96d06b1f9543d6d687fb230465b
marcrheign <mllanto@filamer.edu.ph>
2025-03-24 17:49:19 +0800

    added new file

---
+++ Getting Functional/function.js
@@ -0,0 +1,35 @@
+var dogName = "Marc Rheign Lianto";
+var dogWeight = 10;
+
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF AHHHHH");
+} else {
+    console.log(dogName + " says WOOF WOOF WOOF");
+}
+
+dogName = "Bryan Hyacinth Narte";
+dogWeight = 15;
+
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF AHHHHH");
+} else {
+    console.log(dogName + " says WOOF WOOF WOOF");
+}
+
+dogName = "Ricardo Abao Jr.";
+dogWeight = 14;
+
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF AHHHHH");
+} else {
+    console.log(dogName + " says WOOF WOOF WOOF");
+}
+
+dogName = "Aya Villaruel";
+dogWeight = 12;
+
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF AHHHHH");
+} else {
+    console.log(dogName + " says WOOF WOOF WOOF");
+}
